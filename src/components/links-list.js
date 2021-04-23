2d719a2fb291bf99e18f8f0acfb4382839fcb826
Irina Lukina <ibenderina@gmail.com>
2021-04-23 18:48:06 +0300

    up

--- src/components/links-list.js
+++
@@ -1,9 +0,0 @@
-export default ({link, headline, description, action}) => {
-  return <li className="taplink__list-item">
-            <a className="taplink__list-link" href={link}>
-              <span className="taplink__text">{headline}</span>
-              <span className="taplink__desc">{description}</span>
-              <span className="taplink__action">{action}</span>
-            </a>
-          </li>
-}
--- src/components/social-links.js
+++
@@ -1,7 +0,0 @@
-export default ({link, icon}) => {
-  return <li className="social-list__item">
-          <a className="social-list__link" href={link}>
-            <img className="social-list__icon" src={icon} />
-          </a>
-        </li>
-}
